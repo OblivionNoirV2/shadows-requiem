c33aa2d11336b9e38e29fc641882ac216deee8c2
OblivionNoirV2 <donahueb85@gmail.com>
2023-07-24 16:24:39 -0500

    abort mission

--- bbv2/src/declarations.d.ts
+++ bbv2/src/declarations.d.ts
@@ -9,3 +9,24 @@ declare module "*.mp3" {
     const src: string;
     export default src;
 }
+
+declare module "*.png" {
+    const value: any;
+    export = value;
+}
+
+declare module "*.jpg" {
+    const value: any;
+    export = value;
+}
+
+declare module "*.gif" {
+    const value: any;
+    export = value;
+}
+
+// And so on for any other file types...
+declare module "*.png" {
+    const value: any;
+    export = value;
+}
