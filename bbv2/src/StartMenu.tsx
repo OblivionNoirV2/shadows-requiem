532189ba0744492d3bd6826340d51f8d32d87194
OblivionNoirV2 <donahueb85@gmail.com>
2023-09-05 11:16:27 -0500

    add screen ratio incompatability notice

--- bbv2/src/StartMenu.tsx
+++ bbv2/src/StartMenu.tsx
@@ -63,128 +63,164 @@ const StartMenu: React.FC<StartMenuProps> = ({ on_start, bossStage }) => {
 
     }, [])
 
+    const [isCompatible, setIsCompatible] = useState(true);
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            const aspectRatio = width / height;
+
+            // Replace 1.77 with the aspect ratio you consider to be "desktop"
+            if (aspectRatio < 1.77) {
+                setIsCompatible(false);
+            } else {
+                setIsCompatible(true);
+            }
+        };
+
+        // Initial check
+        handleResize();
+
+        // Listen for window resize events
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
 
     return (
-
-        <main className='flex flex-col mx-auto w-2/3
+        <div>
+            {isCompatible ? (
+                <main className='flex flex-col mx-auto w-2/3
    justify-center items-center space-y-4 mt-12 '>
-            <h1 className='text-8xl glow-ani-text text-black'>影の鎮魂歌</h1>
-            <hr className='bg-black glow-ani-text h-2 w-full border-none'></hr>
-            <h1 className='text-6xl glow-ani-text text-black'>Shadow's Requiem</h1>
-            <div className='flex'>
-                <section className='start-menu space-y-8 flex flex-col'>
-                    <div className='flex flex-col space-y-8'>
+                    <h1 className='text-8xl glow-ani-text text-black'>影の鎮魂歌</h1>
+                    <hr className='bg-black glow-ani-text h-2 w-full border-none'></hr>
+                    <h1 className='text-6xl glow-ani-text text-black'>Shadow's Requiem</h1>
+                    <div className='flex'>
+                        <section className='start-menu space-y-8 flex flex-col'>
+                            <div className='flex flex-col space-y-8'>
 
-                        <StartMenuButton
-                            text='Start Game'
-                            onClick={() => {
-                                on_start();
-                                sfx.playClickSfx();
-                            }}
-                        />
-                        <StartMenuButton
-                            text='Story'
-                            link='/Story'
-                            onClick={sfx.playClickSfx}
-                        />
-                        <StartMenuButton
-                            text='Encyclopedia'
-                            link='/Encyclopedia'
-                            onClick={sfx.playClickSfx}
-                        />
+                                <StartMenuButton
+                                    text='Start Game'
+                                    onClick={() => {
+                                        on_start();
+                                        sfx.playClickSfx();
+                                    }}
+                                />
+                                <StartMenuButton
+                                    text='Story'
+                                    link='/Story'
+                                    onClick={sfx.playClickSfx}
+                                />
+                                <StartMenuButton
+                                    text='Encyclopedia'
+                                    link='/Encyclopedia'
+                                    onClick={sfx.playClickSfx}
+                                />
 
-                        <select className=' bg-[#363040]/60 
+                                <select className=' bg-[#363040]/60 
        py-4 px-3 rounded-2xl text-6xl text-center text-slate-400 glow-ani-border
        diff-select'
-                            onChange={handleDiffChange}
-                            onClick={() => sfx.playClickSfx}
-                            title='Selected difficulty'
-                        >
-                            <option disabled>
-                                Select difficulty...
-                            </option>
-                            <option
-                                title='Reduces boss attack and defense by 50%'
-                                value='very_easy'>
-                                Very Easy
-                            </option>
-                            <option
-                                title='Reduces boss attack and defense by 25%'
-                                value='easy'>
-                                Easy
-                            </option>
-                            <option
-                                selected
-                                title='Standard, no changes'
-                                value='normal'>
-                                Normal
-                            </option>
-                            <option
-                                title='Raises boss attack and defense by 25%'
-                                value='hard'>
-                                Hard
-                            </option>
-                            <option
-                                title='Raises boss attack and defense by 50%'
-                                value='nightmare'>
-                                Nightmare
-                            </option>
-                        </select>
-                        <StartMenuButton
-                            text='Name Characters'
-                            onClick={sfx.playClickSfx}
-                            link='/NameCharacters'
-                        />
-                        <StartMenuButton
-                            text='Credits'
-                            onClick={() => {
-                                handleCreditsClick();
-                                sfx.playClickSfx();
-                            }}
-                        />
-                    </div>
-                </section>
-                {isCreditsShown &&
-                    <section className='flex flex-col m-auto items-center 
+                                    onChange={handleDiffChange}
+                                    onClick={() => sfx.playClickSfx}
+                                    title='Selected difficulty'
+                                >
+                                    <option disabled>
+                                        Select difficulty...
+                                    </option>
+                                    <option
+                                        title='Reduces boss attack and defense by 50%'
+                                        value='very_easy'>
+                                        Very Easy
+                                    </option>
+                                    <option
+                                        title='Reduces boss attack and defense by 25%'
+                                        value='easy'>
+                                        Easy
+                                    </option>
+                                    <option
+                                        selected
+                                        title='Standard, no changes'
+                                        value='normal'>
+                                        Normal
+                                    </option>
+                                    <option
+                                        title='Raises boss attack and defense by 25%'
+                                        value='hard'>
+                                        Hard
+                                    </option>
+                                    <option
+                                        title='Raises boss attack and defense by 50%'
+                                        value='nightmare'>
+                                        Nightmare
+                                    </option>
+                                </select>
+                                <StartMenuButton
+                                    text='Name Characters'
+                                    onClick={sfx.playClickSfx}
+                                    link='/NameCharacters'
+                                />
+                                <StartMenuButton
+                                    text='Credits'
+                                    onClick={() => {
+                                        handleCreditsClick();
+                                        sfx.playClickSfx();
+                                    }}
+                                />
+                            </div>
+                        </section>
+                        {isCreditsShown &&
+                            <section className='flex flex-col m-auto items-center 
            bg-[#363040]/60 py-8 px-4 glow-ani-border rounded-2xl 
           text-xl ml-4 '>
-                        <strong className='flex flex-col space-y-6 text-white'>
-                            <cite>Programmer: Benjamin Donahue</cite>
-                            <cite>Designer: Benjamin Donahue</cite>
-                            <cite>Music: Benjamin Donahue</cite>
-                            <cite>Images: Midjourney AI </cite>
-                            <cite>Volume on symbol: Pixel Perfect</cite>
-                            <cite>Volume off symbol: Google</cite>
-                            <cite className='cite-link'>SFX:
-                                <a href='https://opengameart.org/'
-                                    target='_blank'>
-                                    OpenGameArt.Org,
-                                </a>
-                                <br></br>
-                                <a href='https://www.youtube.com/@SoundEffectsFactory'
-                                    target='_blank'>
-                                    SoundEffectsFactory
-                                </a>
-                                <br></br>
-                                <a href='https://mixkit.co/free-sound-effects/horror/'
-                                    target='_blank'>
-                                    MixKit
-                                </a>
-                                <br></br>
-                                <a href='https://tuna.voicemod.net/sound/d23c1e88-eb51-448e-8ad1-9abde6e2cad7'
-                                    target='_blank'>
-                                    Voicemod
-                                </a>
-                            </cite>
-                        </strong>
+                                <strong className='flex flex-col space-y-6 text-white'>
+                                    <cite>Programmer: Benjamin Donahue</cite>
+                                    <cite>Designer: Benjamin Donahue</cite>
+                                    <cite>Music: Benjamin Donahue</cite>
+                                    <cite>Images: Midjourney AI </cite>
+                                    <cite>Volume on symbol: Pixel Perfect</cite>
+                                    <cite>Volume off symbol: Google</cite>
+                                    <cite className='cite-link'>SFX:
+                                        <a href='https://opengameart.org/'
+                                            target='_blank'>
+                                            OpenGameArt.Org,
+                                        </a>
+                                        <br></br>
+                                        <a href='https://www.youtube.com/@SoundEffectsFactory'
+                                            target='_blank'>
+                                            SoundEffectsFactory
+                                        </a>
+                                        <br></br>
+                                        <a href='https://mixkit.co/free-sound-effects/horror/'
+                                            target='_blank'>
+                                            MixKit
+                                        </a>
+                                        <br></br>
+                                        <a href='https://tuna.voicemod.net/sound/d23c1e88-eb51-448e-8ad1-9abde6e2cad7'
+                                            target='_blank'>
+                                            Voicemod
+                                        </a>
+                                    </cite>
+                                </strong>
 
-                    </section>
+                            </section>
 
-                }
-            </div>
+                        }
+                    </div>
 
 
-        </main>
+                </main>
+
+            ) : (
+                <h1 className='text-5xl text-white justify-center flex w-1/2 mx-auto'>
+                    Sorry, this game is only intended for desktops and laptops.
+                </h1>
+            )}
+        </div>
     );
 };
 
